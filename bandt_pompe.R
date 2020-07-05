446ec18ab9da000f334e1b04a0d86585c3641e17
Joao Borges <joaoborges@dct.ufrn.br>
2020-07-05 15:12:52 -0300

    Returning loading required packages from the main file

--- bandt_pompe.R
+++ bandt_pompe.R
@@ -2,7 +2,14 @@
 
 # Implementation of the Bandt-Pompe transformations
 
-source('includes.R')
+# required packages
+
+suppressMessages(library(igraph))
+
+require(e1071, quietly=TRUE)
+
+suppressMessages(library(entropy))
+
 
 ###################
 # regular functions
